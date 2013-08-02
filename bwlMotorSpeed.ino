75d73c8f3480c953bdd925ae0b3ffeeecb4cf905
JChristensen <j.christensen@sbcglobal.net>
2013-08-02 09:41:01 -0400

    Change to state machine architecture, add test mode.

--- bwlMotorSpeed.ino
+++ bwlMotorSpeed.ino
@@ -6,28 +6,51 @@
 //Arduino IDE v1.0.5.
 //Jack Christensen 13Jun2013
 //v1.1 31Jul2013 -- Added automatic pedal calibration, quantization of PWM output.
+//v1.2 01Aug2013 -- Change to state machine architecture. Add button to select modes.
 
-#include <Streaming.h>                  //http://arduiniana.org/libraries/streaming/
-#include <movingAvg.h>                  //http://github.com/JChristensen/movingAvg
 #include <avr/eeprom.h>
+#include <Button.h>                     //http://github.com/JChristensen/Button
+#include <movingAvg.h>                  //http://github.com/JChristensen/movingAvg
+#include <Streaming.h>                  //http://arduiniana.org/libraries/streaming/
 
-#define SAMPLE_SIZE 20
-#define PAUSE_BETWEEN_SAMPLES 0         //milliseconds
-#define BAUD_RATE 115200
-#define PWM_OUT 3                       //pin for PWM motor drive
-#define CALIBRATION_INTERVAL 10000      //milliseconds at startup for pedal calibration
-#define CALIB_DIFF_MIN 100              //minimum difference in ADC readings to recalibrate pedals
-#define CALIB_SAVE_MIN 5                //minimum difference to resave a limit in EEPROM
+//pin definitions
+const int PWM_OUT = 3;                  //PWM for MOSFET motor drive
+const int RED_LED = 8;                  //PB0
+const int GRN_LED = 9;                  //PB1
+const int MODE_BUTTON = 10;
 
+//fast port manipulation macros for LEDs
+#define RED_LED_ON PORTB |= _BV(PORTB0)
+#define RED_LED_OFF PORTB &= ~_BV(PORTB0)
+#define RED_LED_TOGGLE PINB |= _BV(PORTB0)
+#define GRN_LED_ON PORTB |= _BV(PORTB1)
+#define GRN_LED_OFF PORTB &= ~_BV(PORTB1)
+#define GRN_LED_TOGGLE PINB |= _BV(PORTB1)
+
+//other constants
+const long BAUD_RATE = 115200;
+const int CALIB_DIFF_MIN = 100;                      //minimum difference in ADC readings needed to recalibrate pedals
+const int CALIB_SAVE_MIN = 3;                        //minimum difference in a single calibration values to resave in EEPROM
+const boolean BTN_PULLUP = true;                     //turn on pullup resistor for buttons
+const boolean BTN_INVERT = true;                     //inverted logic for buttons with pullups
+const unsigned long BTN_DEBOUNCE = 25;               //milliseconds
+const unsigned long SLOW_BLINK = 1000;               //milliseconds
+const unsigned long FAST_BLINK = 100;                //milliseconds
+
+//globals
 volatile boolean adcBusy;
 volatile int accelVal;
-uint16_t accel, accelAvg, accelMin, accelMax, accelNewMin=9999, accelNewMax;
 uint16_t EEMEM EEP_accelMin, EEP_accelMax;
+    Button btnMode(MODE_BUTTON, BTN_PULLUP, BTN_INVERT, BTN_DEBOUNCE);    //mode button
 
 void setup(void)
 {
     delay(1000);
     Serial.begin(BAUD_RATE);
+    
+    //pin initialization
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GRN_LED, OUTPUT);
 
     //set up the timer
     TCCR1B = 0;                         //stop the timer
@@ -46,66 +69,125 @@ void setup(void)
     ADCSRA  = _BV(ADEN)  | _BV(ADATE) | _BV(ADIE);     //enable ADC, auto trigger, interrupt when conversion complete
     ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);    //ADC prescaler: divide by 128
     ADCSRB = _BV(ADTS2) | _BV(ADTS0);                  //trigger ADC on Timer/Counter1 Compare Match B
-    
-    //pedal calibration
-    Serial << F("***PEDAL CALIBRATION") << endl;
-    while (millis() <= CALIBRATION_INTERVAL) {
-        while (!adcBusy);               //wait for next conversion to start
-        while (adcBusy);                //wait for ADC conversion to complete
-        cli();
-        accel = accelVal;               //save the analog reading
-        sei();
-        if (accel < accelNewMin) accelNewMin = accel;
-        if (accel > accelNewMax) accelNewMax = accel;
-    }
-    
-    //get the saved values from eeprom
-    accelMin = eeprom_read_word(&EEP_accelMin);
-    accelMax = eeprom_read_word(&EEP_accelMax);
-    Serial << F("Saved accel limits: ") << accelMin << ' ' << accelMax << endl;
-    Serial << F("New accel calibration values: ") << accelNewMin << ' ' << accelNewMax << endl;
-    if (accelNewMax - accelNewMin >= CALIB_DIFF_MIN) {
-        if (abs(accelNewMin - accelMin) >= CALIB_SAVE_MIN) {
-            accelMin = accelNewMin;
-            eeprom_write_word(&EEP_accelMin, accelMin);
-            Serial << F("accelMin updated") << endl;
-        }
-        if (abs(accelNewMax - accelMax) >= CALIB_SAVE_MIN) {
-            accelMax = accelNewMax;
-            eeprom_write_word(&EEP_accelMax, accelMax);
-            Serial << F("accelMax updated") << endl;
-        }
-    }
-    else {
-        Serial << F("Invalid accel calibration input, no change.") << endl;
-    }
-    Serial << F("***END CALIBRATION") << endl;
 }
+
+enum STATES_t {INIT, CALIBRATE, TEST, RUN, STOP} STATE;
 
 void loop(void)
 {
-    unsigned long tStart;               //timer interrupt time from micros()
-    unsigned long tEnd;                 //adc interrupt time from micros()
+    static uint16_t accel, accelAvg, accelMin, accelMax, accelNewMin=9999, accelNewMax;
+    static unsigned long ms, msLast;
     movingAvg avgAccel;
     
-    while (!adcBusy);                   //wait for next conversion to start
-//    tStart = micros();                  //capture the time
-    while (adcBusy);                    //wait for ADC conversion to complete
-//    tEnd = micros();                    //capture the time
-    cli();
-    accel = accelVal;                   //save the analog reading
-    sei();
-    accelAvg = avgAccel.reading(accel);
-    //quantize the accelAvg value to 17 steps: 0, 15, 31, 47, 63, 79, ... 223, 239, 255.
-    accelAvg = constrain(map(accelAvg, accelMin, accelMax, 0, 17), 0, 16);
-    if (accelAvg > 0) accelAvg = accelAvg * 16 - 1;
-    analogWrite(PWM_OUT, accelAvg);
-//    Serial << accel << ' ' << accelAvg << endl;
-//    Serial.flush();
-
-    #if PAUSE_BETWEEN_SAMPLES > 0
-    delay(PAUSE_BETWEEN_SAMPLES);
-    #endif
+    ms = millis();
+    btnMode.read();
+    
+    switch (STATE) {
+        
+        case INIT:
+            //get the saved pedal calibration values from eeprom
+            accelMin = eeprom_read_word(&EEP_accelMin);
+            accelMax = eeprom_read_word(&EEP_accelMax);
+        
+            if (btnMode.isPressed()) {
+                STATE = CALIBRATE;
+                RED_LED_ON;
+                GRN_LED_OFF;
+                while (btnMode.isPressed()) btnMode.read();    //wait for user to release the button
+                Serial << F("***PEDAL CALIBRATION") << endl;
+                Serial << F("Existing accel limits: ") << accelMin << ' ' << accelMax << endl;
+            }
+            else {
+                STATE = RUN;
+                RED_LED_OFF;
+                GRN_LED_ON;
+            }
+            break;
+        
+        case CALIBRATE:                     //pedal calibration
+            if (btnMode.wasReleased()) {
+                Serial << F("New accel limits from calibration: ") << accelNewMin << ' ' << accelNewMax << endl;
+                if (accelNewMax - accelNewMin >= CALIB_DIFF_MIN) {
+                    if (abs(accelNewMin - accelMin) >= CALIB_SAVE_MIN) {
+                        accelMin = accelNewMin;
+                        eeprom_write_word(&EEP_accelMin, accelMin);
+                        Serial << F("accelMin updated") << endl;
+                    }
+                    if (abs(accelNewMax - accelMax) >= CALIB_SAVE_MIN) {
+                        accelMax = accelNewMax;
+                        eeprom_write_word(&EEP_accelMax, accelMax);
+                        Serial << F("accelMax updated") << endl;
+                    }
+                }
+                else {
+                    Serial << F("Invalid accel calibration input, no change.") << endl;
+                }
+                Serial << F("***END CALIBRATION") << endl;
+                RED_LED_OFF;
+                GRN_LED_ON;
+                STATE = RUN;
+            }
+            else {
+                while (!adcBusy);               //wait for next conversion to start
+                while (adcBusy);                //wait for ADC conversion to complete
+                cli();
+                accel = accelVal;               //save the analog reading
+                sei();
+                if (accel < accelNewMin) accelNewMin = accel;
+                if (accel > accelNewMax) accelNewMax = accel;
+                RED_LED_TOGGLE;
+            }
+            break;
+            
+        case TEST:
+            if (!runTest(false)) {
+                STATE = STOP;
+                GRN_LED_ON;
+                RED_LED_OFF;
+            }
+            if (ms - msLast >= FAST_BLINK) {
+                msLast = ms;
+                GRN_LED_TOGGLE;
+                RED_LED_TOGGLE;
+            }
+            if (btnMode.wasReleased()) {
+                runTest(true);            //abort test mode
+                GRN_LED_ON;
+                RED_LED_OFF;
+                STATE = RUN;
+            }
+            break;
+            
+        case RUN:
+            while (!adcBusy);                   //wait for next conversion to start
+            while (adcBusy);                    //wait for ADC conversion to complete
+            cli();
+            accel = accelVal;                   //save the analog reading
+            sei();
+            accelAvg = avgAccel.reading(accel);
+            //quantize the accelAvg value to 17 steps: 0, 15, 31, 47, 63, 79, ... 223, 239, 255.
+            accelAvg = constrain(map(accelAvg, accelMin, accelMax, 0, 17), 0, 16);
+            if (accelAvg > 0) accelAvg = accelAvg * 16 - 1;
+            analogWrite(PWM_OUT, accelAvg);
+        //    Serial << accel << ' ' << accelAvg << endl;
+        //    Serial.flush();
+            GRN_LED_TOGGLE;
+            if (btnMode.wasReleased()) {
+                GRN_LED_ON;
+                RED_LED_ON;
+                msLast = ms;
+                STATE = TEST;
+            }
+            break;
+        
+        case STOP:    //Abandon all hope, ye who enter here (no exit from STOP state but a reset)
+            if (ms - msLast >= SLOW_BLINK) {
+                msLast = ms;
+                GRN_LED_TOGGLE;
+                RED_LED_TOGGLE;
+            }
+            break;
+    }
 }
 
 ISR(ADC_vect)
---
+++ testMode.ino
@@ -0,0 +1,75 @@
+//test mode timing constants
+const unsigned long TEST_ON_DUR = 5000;              //ms on
+const unsigned long TEST_OFF_DUR = 5000;             //ms off
+const unsigned int TEST_REPEAT = 16;                 //number of on/off cycles
+const unsigned long TEST_REST_DUR = 20000;           //ms to rest (off) between sets of on/off cycles
+const unsigned long TEST_DURATION = 60 * 1;          //in minutes, e.g. 60 * 8 = 8 hours
+
+enum TEST_STATES_t {TEST_INIT, TEST_ON, TEST_OFF, TEST_REST} TEST_STATE;
+
+//returns false when TEST_DURATION has expired, else returns true.
+//abort = true stops test prematurely.
+boolean runTest(boolean abort)
+{
+    static unsigned long ms, msLast, testStart;
+    static unsigned int nCycle;
+    
+    ms = millis();
+    
+    if (abort) {
+        analogWrite(PWM_OUT, 0);
+        TEST_STATE = TEST_INIT;
+        return false;
+    }
+    
+    switch (TEST_STATE) {
+        
+        case TEST_INIT:
+            nCycle = 0;
+            testStart = ms;
+            msLast = ms;
+            analogWrite(PWM_OUT, 255);
+            TEST_STATE = TEST_ON;
+            ++nCycle;
+            break;
+        
+        case TEST_ON:
+            if (ms - msLast >= TEST_ON_DUR) {
+                msLast = ms;
+                if (nCycle >= TEST_REPEAT) {
+                    nCycle = 0;
+                    analogWrite(PWM_OUT, 0);
+                    TEST_STATE = TEST_REST;
+                }
+                else {
+                    analogWrite(PWM_OUT, 0);
+                    TEST_STATE = TEST_OFF;
+                }
+            }
+            break;
+        
+        case TEST_OFF:
+            if (ms - msLast >= TEST_OFF_DUR) {
+                msLast = ms;
+                analogWrite(PWM_OUT, 255);
+                TEST_STATE = TEST_ON;
+                ++nCycle;
+            }
+            break;
+        
+        case TEST_REST:
+            if (ms - testStart >= TEST_DURATION * 60000UL) {
+                analogWrite(PWM_OUT, 0);
+                TEST_STATE = TEST_INIT;
+                return false;
+            }                
+            else if (ms - msLast >= TEST_REST_DUR) {
+                msLast = ms;
+                analogWrite(PWM_OUT, 255);
+                TEST_STATE = TEST_ON;
+                ++nCycle;
+            }
+            break;
+    }
+    return true;
+}
